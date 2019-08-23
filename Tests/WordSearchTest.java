5cd9b20aad190e3e675745296ede78629454331a
Ansel Landini <ansel-landini@outlook.com>
2019-08-22 20:37:28 -0500

    Second test for reading in words to find.

--- Tests/WordSearchTest.java
+++ Tests/WordSearchTest.java
@@ -2,6 +2,7 @@ import com.auburnpeaks.WordSearch;
 import org.junit.*;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class WordSearchTest {
 
@@ -10,4 +11,11 @@ public class WordSearchTest {
         WordSearch wordsearch = new WordSearch();
         assertEquals("BANANA", wordsearch.readTestPuzzle("Inputs/WordSearchTestPuzzle1")[0]);
     }
+
+    @Test
+    public void CheckThatAllWordsFromTest1AreRead(){
+        WordSearch wordsearch = new WordSearch();
+        assertArrayEquals(new String[]{"BANANA", "CAT", "OWL", "PANTHER"}, wordsearch.readTestPuzzle("Inputs/WordSearchTestPuzzle1"));
+    }
+
 }
--- src/com/auburnpeaks/WordSearch.java
+++ src/com/auburnpeaks/WordSearch.java
@@ -1,9 +1,20 @@
 package com.auburnpeaks;
 
+import java.io.*;
+
 public class WordSearch {
 
     public String[] readTestPuzzle(String pathToTestPuzzle){
-        return new String[]{"BANANA","CATS"};
+        String[] wordsToFind = new String[]{};
+
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(pathToTestPuzzle));
+            wordsToFind = in.readLine().split(",");
+            return wordsToFind;
+        }
+        catch(Exception e){
+            return wordsToFind;
+        }
     }
 
 }
