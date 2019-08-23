0edc2d33c8e6dadbc505091ed634fe3c83ca1bcc
Ansel Landini <ansel-landini@outlook.com>
2019-08-22 21:36:44 -0500

    Test case 3 and read in puzzle.

--- Tests/WordSearchTest.java
+++ Tests/WordSearchTest.java
@@ -5,19 +5,27 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
 public class WordSearchTest {
+    private WordSearch wordsearch;
+
+    @Before
+    public void setUp(){
+        wordsearch  = new WordSearch();
+        wordsearch.readTestPuzzle("Inputs/WordSearchTestPuzzle1");
+    }
 
     @Test
     public void CheckThatFirstWordFromTest1IsBanana(){
-        WordSearch wordsearch = new WordSearch();
-        wordsearch.readTestPuzzle("Inputs/WordSearchTestPuzzle1");
         assertEquals("BANANA", wordsearch.getWordsToFind()[0]);
     }
 
     @Test
     public void CheckThatAllWordsFromTest1AreRead(){
-        WordSearch wordsearch = new WordSearch();
-        wordsearch.readTestPuzzle("Inputs/WordSearchTestPuzzle1");
         assertArrayEquals(new String[]{"BANANA", "CAT", "OWL", "PANTHER"}, wordsearch.getWordsToFind());
+    }
+
+    @Test
+    public void CheckThatAllPuzzleLinesFromTest1AreRead(){
+        assertEquals(9, wordsearch.getPuzzle().size());
     }
 
 }
--- src/com/auburnpeaks/WordSearch.java
+++ src/com/auburnpeaks/WordSearch.java
@@ -1,15 +1,26 @@
 package com.auburnpeaks;
-
+import java.util.ArrayList;
 import java.io.*;
+import java.util.Arrays;
 
 public class WordSearch {
     private String[] wordsToFind = new String[]{};
+    private ArrayList<ArrayList<String>> wordSearchPuzzle = new ArrayList<>();
 
     public void readTestPuzzle(String pathToTestPuzzle){
 
         try{
             BufferedReader in = new BufferedReader(new FileReader(pathToTestPuzzle));
             wordsToFind = in.readLine().split(",");
+            String nextLine = in.readLine();
+            while(nextLine != null){
+                String[] wordSearchStringArray = nextLine.split(",");
+
+                ArrayList<String> wordSearchRow = new ArrayList<String>(Arrays.asList(wordSearchStringArray));
+                wordSearchPuzzle.add(wordSearchRow);
+
+                nextLine = in.readLine();
+            }
         }
         catch(Exception e){
             System.out.println("Error Reading File:" + pathToTestPuzzle);
@@ -18,6 +29,10 @@ public class WordSearch {
 
     public String[] getWordsToFind(){
         return wordsToFind;
+    }
+
+    public ArrayList<ArrayList<String>> getPuzzle(){
+        return wordSearchPuzzle;
     }
 
 }
