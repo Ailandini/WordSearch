9133002a5a56a341282b7225e4f3c01288698ad5
Ansel Landini <ansel-landini@outlook.com>
2019-08-26 23:14:37 -0500

    Added run task to let gradle take command line input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length < 1){
+            System.out.println("Must provide a Word Search input file: gradle run -path to file-");
+        }
+        if(args.length > 1){
+            System.out.println("Word Search can only solve one puzzle at a time.");
+        }
         WordSearch wordsearchSingle = new WordSearch();
-        wordsearchSingle.readTestPuzzle("Inputs/WordSearchSingleWord");
+        wordsearchSingle.readTestPuzzle(args[0]);
         wordsearchSingle.findWords();
         System.out.println(wordsearchSingle.getFoundWords());
     }
